ee3f40ee6caaaa54e2892adaec5d4cf6582634d7
Guilherme <guivjjig@gmail.com>
2025-02-21 10:47:06 -0300

    first commit

---
+++ aula1.js
@@ -0,0 +1,11 @@
+const valor1 = 12;
+const valor2 = 5;
+let soma = valor1 + valor2;
+let sub = valor1 - valor2;
+let mult = valor1 * valor2;
+let div = valor1 / valor2;
+
+console.log(" A soma e:" + soma);
+console.log("A subtracao e:" + sub);
+console.log("A multiplicao e:" + mult)
+console.log("A divisao e:"+ div)
