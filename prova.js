9c06eccad22d567faf23dff05ccdfd1977ce8be1
Guilherme <guivjjig@gmail.com>
2025-02-24 12:21:21 -0300

    ooo

---
+++ prova.js
@@ -0,0 +1,23 @@
+let av1= 5.0;
+let av2= 6.0;
+let rec1= 7.0;
+let rec2= 3.0;
+let nt1
+let nt2
+if(rec1 > av1 ){
+    nt1=rec1
+ }else{
+    nt1
+ }
+ if(rec2 > av2 ){
+    nt2=rec2
+ }else{
+    nt2
+ }
+ if(nt1+nt2 >=6.0){
+    console.log("voce passo ")
+ }else{
+    console.log ("voce reprovou")
+
+ }
+ 
