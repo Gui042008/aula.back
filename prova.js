7ca8ebdcc06fe2014ad151699b78242a5b140a31
Guilherme <guivjjig@gmail.com>
2025-02-26 10:39:00 -0300

    ooiiii

--- prova.js
+++ prova.js
@@ -1,23 +1,25 @@
 let av1= 5.0;
-let av2= 6.0;
-let rec1= 7.0;
+let av2= 0.0;
+let rec1= 1.0;
 let rec2= 3.0;
-let nt1
-let nt2
+let nt1;
+let nt2;
+
 if(rec1 > av1 ){
     nt1=rec1
  }else{
-    nt1
+    nt1=av1
  }
  if(rec2 > av2 ){
     nt2=rec2
  }else{
-    nt2
+    nt2=av2
  }
- if(nt1+nt2 >=6.0){
-    console.log("voce passo ")
+ let total =nt1+nt2
+ if( total/2 >=6.0){
+    console.log("voce passo sua media foi " + total/2)
  }else{
-    console.log ("voce reprovou")
+    console.log ("voce reprovou sua media foi " + total/2)
 
  }
  
