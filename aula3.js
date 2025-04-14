0096df26aca93fc68ead852e19bcf95175cadd2f
kauany <kauany.taborda@escola.pr.gov.br>
2025-04-14 11:38:42 -0300

    oooooooooooo

--- aula3.js
+++ aula3.js
@@ -1,20 +1,34 @@
-const livros = require ("./livrosTexto")
+const livros = require("./Livros.json");
+
 function mostraLivros() {
     livros.forEach((livro) => {
         console.log(`${livro.titulo} - ${livro.autor} - Ano: ${livro.ano} - Páginas: ${livro.paginas} - Preço: R$ ${livro.preco.toFixed(2)}`);
     });
 }
 
-//Gera um arquivo JSON dos livros
-function criarArquivo(){
-    let livrosTexto = JSON.stringify(livros);
-    const fs = require ('fs');
-    fs.writeFileSync("Livros.json", livrosTexto)
-
-
-
+function addLivro(titulo, autor, editora, ano, sinopse, paginas, genero, preco) {
+    livros.push({
+        titulo: titulo,
+        autor: autor,
+        editora: editora,
+        ano: ano,
+        sinopse: sinopse,
+        paginas: paginas,
+        genero: genero,
+        preco: preco
+    });
+    console.log("Livro adicionado com sucesso!!");
 }
 
-//criarArquivo();
+
+addLivro("Tigrinho", "Guilherme Rodrigues", "Luiz Coelho e CIA", 2020, "Um tigrinho que gosta de dinheiro", 1, ["Roubo", "Falsidade", "Mentira"], 600);
+
+function criarArquivo() {
+    const fs = require('fs');
+    const livrosTexto = JSON.stringify(livros, null, 2); 
+    fs.writeFileSync("Livros.json", livrosTexto);
+}
+
+
 
 mostraLivros();
